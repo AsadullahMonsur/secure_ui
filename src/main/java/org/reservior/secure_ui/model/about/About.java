43eded68bd877794b4b9bdba544cc3e4ac1b6bdb
cinnamon <asadullahmonsur321@gmail.com>
2024-05-09 04:50:24 +0900

    update 05

---
+++ src/main/java/org/reservior/secure_ui/model/about/About.java
@@ -0,0 +1,17 @@
+package org.reservior.secure_ui.model.about;
+
+public class About {
+    private String image;
+
+    public About(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
